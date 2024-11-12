ca9ad3f73ce07e30fbe49e89e55412e1281e4798
raysasitiaisyah <sulis.pitri2007@smkn4-tsm.sch.id>
2024-11-12 13:06:52 +0700

    menambahkan login dan register

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('usertype')->default('user');// admin, ptk, siswa
+            $table->timestamp('email_varified_at')->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginRegisterController;
+use App\Http\Controllers\Admin\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [LoginRegisterController::class, 'register'])->name('register');
+    Route::post('/store', [LoginRegisterController::class, 'store'])->name('store');
+    Route::get('/login', [LoginRegisterController::class, 'login'])->name('login');
+    Route::post('/authenticate', [LoginRegisterController::class, 'authenticate'])->name('authenticate');
+});
+
+Route::middleware('auth', 'admin')->group(function () {
+    Route::get('admin/dashboard', [adminController::class, 'index'])->name('admin/dashboard');
+    Route::post('/logout', [LoginRegisterController::class, 'logout'])->name('logout');
 });
